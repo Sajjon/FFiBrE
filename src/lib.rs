d5c1a3f7ce616b43860da6bccd856f24992e7a53
Alexander Cyon <alex.cyon@gmail.com>
2024-02-01 20:03:38 +0100

    channels

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
-use rust_decimal::prelude::*;
 use serde::{Deserialize, Serialize};
 use serde_json::to_vec;
-use std::{collections::HashMap, sync::Arc};
-use tokio::sync::{Semaphore, TryAcquireError};
-use url::Url;
+use std::{collections::HashMap, os::unix::thread, sync::Arc};
 
 #[uniffi::export]
 pub trait NetworkRequest: Send + Sync {
@@ -13,8 +10,9 @@ pub trait NetworkRequest: Send + Sync {
     fn http_header_fields(&self) -> HashMap<String, String>;
 }
 
+#[derive(uniffi::Record)]
 struct NetworkRequestImpl {
-    url: Url,
+    url: String,
     body: Vec<u8>,
     method: String,
     headers: HashMap<String, String>,
@@ -42,7 +40,42 @@ pub trait NetworkResponse: NetworkRequest {
     fn status_code(&self) -> u16;
 }
 
-#[derive(Debug, PartialEq, Eq, thiserror::Error, uniffi::Error)]
+#[derive(uniffi::Record, Clone)]
+pub struct NetworkResponseImpl {}
+impl NetworkRequest for NetworkResponseImpl {
+    fn url(&self) -> String {
+        todo!()
+    }
+
+    fn http_body(&self) -> Vec<u8> {
+        todo!()
+    }
+
+    fn http_method(&self) -> String {
+        todo!()
+    }
+
+    fn http_header_fields(&self) -> HashMap<String, String> {
+        todo!()
+    }
+}
+impl NetworkResponse for NetworkResponseImpl {
+    fn status_code(&self) -> u16 {
+        todo!()
+    }
+}
+impl NetworkResponseImpl {
+    pub fn new() -> Self {
+        todo!()
+    }
+}
+impl NetworkRequestImpl {
+    pub fn new(url: String) -> Self {
+        todo!()
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, thiserror::Error, uniffi::Error)]
 pub enum Error {
     #[error("Bad code")]
     BadResponseCode,
@@ -50,110 +83,196 @@ pub enum Error {
     #[error("No XRD balance found in entity state response")]
     NoXRDBalanceFound,
 
-    #[error("Failed to parse decimal from amount string {0}")]
-    FailedToParseDecimalFromAmountString { amount: String },
+    #[error("Unable to JSON deserialize HTTP response body into type: {type_name}")]
+    UnableJSONDeserializeHTTPResponseBodyIntoTypeName { type_name: String },
 }
 
 #[uniffi::export]
-pub trait GotNetworkResponse: Send + Sync {
-    fn provide_response(&self, response: Arc<dyn NetworkResponse>);
+pub trait HTTPClientRequestSender: Send + Sync {
+    /// Called by Rust, Swift side makes call, and then
+    /// Swift side SHOULD call `got_response` method
+    /// on `HTTPClient`
+    fn send(&self, request: NetworkRequestImpl) -> Result<(), Error>;
 }
 
-type Callback = fn(Arc<dyn NetworkResponse>);
-struct GotNetworkResponseImpl {
-    callback: Callback,
-}
-impl GotNetworkResponse for GotNetworkResponseImpl {
-    fn provide_response(&self, response: Arc<dyn NetworkResponse>) {
-        (self.callback)(response)
-    }
+#[derive(uniffi::Enum, Clone)]
+pub enum NetworkResult {
+    Success { value: NetworkResponseImpl },
+    Failure { error: Error },
 }
 
 #[uniffi::export]
-pub trait NetworkRequestMaker: Send + Sync {
-    fn make_request(
-        &self,
-        request: Arc<dyn NetworkRequest>,
-        on_response: Arc<dyn GotNetworkResponse>,
-    ) -> Result<(), Error>;
+pub trait HTTPClientNetworkResultReceiver: Send + Sync {
+    /// Called by Rust, Swift side makes call, and then
+    /// Swift side SHOULD call `got_result` method
+    /// on `HTTPClient`
+    fn got_result(&self, result: NetworkResult, for_request: NetworkRequestImpl);
 }
 
-const xrd_resource_address: String =
-    "resource_rdx1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxradxrd";
+const XRD: &str = "resource_rdx1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxradxrd";
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct FungibleResourceItem {
     amount: String,
     resource_address: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct FungibleResources {
     items: Vec<FungibleResourceItem>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct EntityStateItem {
     fungible_resources: FungibleResources,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 struct EntityState {
     items: Vec<EntityStateItem>,
 }
 
-fn parse_xrd_balance_from(entity_state: EntityState) -> Result<Decimal, Error> {
+fn parse_xrd_balance_from(entity_state: EntityState) -> Result<String, Error> {
     assert_eq!(entity_state.items.len(), 1);
     let item: &EntityStateItem = entity_state.items.first().unwrap();
-    let fungible_resources = item.fungible_resources;
+    let fungible_resources = item.fungible_resources.clone();
 
     fungible_resources
         .items
-        .iter()
-        .filter(|x| x.resource_address == xrd_resource_address)
-        .map(|x| x.amount)
+        .into_iter()
+        .filter(|x| x.resource_address == XRD)
+        .map(|x| x.amount.clone())
         .next()
         .ok_or(Error::NoXRDBalanceFound)
-        .and_then(|s| {
-            s.parse::<Decimal>()
-                .map_err(|_| Error::FailedToParseDecimalFromAmountString { amount: s.clone() })
-        })
 }
 
 #[derive(Serialize)]
 struct GetEntityDetailsRequest {
     addresses: Vec<String>,
 }
+impl GetEntityDetailsRequest {
+    pub fn new(address: impl AsRef<str>) -> Self {
+        Self {
+            addresses: vec![address.as_ref().to_owned()],
+        }
+    }
+}
 
-// curl -X POST https://mainnet.radixdlt.com/state/entity/details -H 'Content-Type: application/json' -d '{"addresses": ["account_rdx16xlfcpp0vf7e3gqnswv8j9k58n6rjccu58vvspmdva22kf3aplease"]}'
+// #[derive(uniffi::Object)]
+pub struct HTTPClient {
+    // sender: tokio::sync::mpsc::UnboundedSender<NetworkRequestImpl>,
+    // receiver: tokio::sync::mpsc::UnboundedReceiver<NetworkResult>,
+    request_sender: Arc<dyn HTTPClientRequestSender>,
+    result_receiver: Arc<dyn HTTPClientNetworkResultReceiver>, // sender: std::sync::mpsc::SyncSender<NetworkRequestImpl>,
+                                                               // receiver: std::sync::mpsc::Receiver<NetworkResult>
+}
+
+// #[uniffi::export]
+impl HTTPClient {
+    // #[uniffi::constructor]
+    pub fn new(
+        request_sender: Arc<dyn HTTPClientRequestSender>,
+        result_receiver: Arc<dyn HTTPClientNetworkResultReceiver>,
+    ) -> Self {
+        // let (sender, receiver) = tokio::sync::mpsc::unbounded_channel();
+
+        // let (sender, receiver) = std::sync::mpsc::sync_channel(1);
+        // Self { receiver, sender }
+        Self {
+            request_sender,
+            result_receiver,
+        }
+    }
+}
+
+impl HTTPClient {
+    async fn make_request(
+        &self,
+        request: NetworkRequestImpl,
+    ) -> Result<NetworkResponseImpl, Error> {
+        // self.request_sender.send(request);
+        // self.sender.send(request).unwrap();
+        // loop {
+        //     self.receiver.recv()
+        // }
+        self.request_sender.send(request).unwrap();
+
+        self.result_receiver.
+    }
+}
+
+#[derive(uniffi::Object)]
+pub struct GatewayClient {
+    http_client: Arc<HTTPClient>,
+}
+impl GatewayClient {
+    async fn make_request<T, U, V, F>(
+        &self,
+        path: impl AsRef<str>,
+        method: impl AsRef<str>,
+        request: T,
+        map: F,
+    ) -> Result<V, Error>
+    where
+        T: Serialize,
+        U: for<'a> Deserialize<'a>,
+        F: Fn(U) -> Result<V, Error>,
+    {
+        let body = to_vec(&request).unwrap();
+        let url = format!("https://mainnet.radixdlt.com/{}", path.as_ref());
+        let request = NetworkRequestImpl {
+            url,
+            body,
+            method: method.as_ref().to_owned(),
+            headers: HashMap::<String, String>::from_iter([(
+                "Content-Type".to_owned(),
+                "application/json".to_owned(),
+            )]),
+        };
+        let response = self.http_client.make_request(request).await;
+        response
+            .and_then(|r| {
+                serde_json::from_slice::<U>(&r.http_body()).map_err(|_| {
+                    Error::UnableJSONDeserializeHTTPResponseBodyIntoTypeName {
+                        type_name: std::any::type_name::<U>().to_owned(),
+                    }
+                })
+            })
+            .and_then(|s| map(s))
+    }
+
+    async fn get<T, U, V, F>(&self, path: impl AsRef<str>, request: T, map: F) -> Result<V, Error>
+    where
+        T: Serialize,
+        U: for<'a> Deserialize<'a>,
+        F: Fn(U) -> Result<V, Error>,
+    {
+        self.make_request(path, "GET", request, map).await
+    }
+}
+
+impl GatewayClient {
+    pub fn new_with_http_client(http_client: Arc<HTTPClient>) -> Self {
+        Self { http_client }
+    }
+}
+
 #[uniffi::export]
-pub async fn get_xrd_balance_of_account(
-    address: String,
-    request_maker: Arc<dyn NetworkRequestMaker>,
-) -> Result<String, Error> {
-    let body_request = GetEntityDetailsRequest {
-        addresses: vec![address],
-    };
-    let body = to_vec(&body_request).unwrap();
-    let request = NetworkRequestImpl {
-        url: "https://mainnet.radixdlt.com/state/entity/details"
-            .parse()
-            .unwrap(),
-        body,
-        method: "POST".to_string(),
-        headers: HashMap::<String, String>::from_iter([(
-            "Content-Type".to_owned(),
-            "application/json".to_owned(),
-        )]),
-    };
+impl GatewayClient {
+  
+    #[uniffi::constructor]
+    pub fn new(request_sender: Arc<dyn HTTPClientRequestSender>, result_receiver: Arc<dyn HTTPClientNetworkResultReceiver>) -> Self {
+        Self::new_with_http_client(HTTPClient::new(request_sender, result_receiver).into())
+    }
 
-    let response_listener = GotNetworkResponseImpl {
-        callback: |response| println!("Got response - status code: {}", response.status_code()),
-    };
-
-    request_maker.make_request(Arc::new(request), Arc::new(response_listener));
-
-    todo!("how to blocking wait for `callback` in `GotNetworkResponseImpl`? We need a 'promise handle' basically...")
+    pub async fn get_xrd_balance_of_account(&self, address: String) -> Result<String, Error> {
+        self.get(
+            "state/entity/details",
+            GetEntityDetailsRequest::new(address),
+            parse_xrd_balance_from,
+        )
+        .await
+    }
 }
 
 uniffi::include_scaffolding!("network");
--- tests/test.swift
+++ tests/test.swift
@@ -1,6 +1,13 @@
+import Foundation
 
 func test() throws {
     print("HELLO WORLD from swift")
+
+    let session = URLSession.shared
+    
+    struct HTTPClient: NetworkRequestMaker {
+        func makeRequest(request: NetworkRequest)
+    }
 }
 
 try! test()

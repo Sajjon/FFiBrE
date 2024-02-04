4be953b5abff9745ad3b88c450003dceb3e706a6
Alexander Cyon <alex.cyon@gmail.com>
2024-02-04 16:53:41 +0100

    split tests

--- tests/test_file_io.swift
+++ tests/test_file_io.swift
@@ -1,9 +1,9 @@
 import Foundation
 import ffibre
 
-extension FfiNetworkingError: Swift.Error {} /* Some bug in UniFFI... */
-extension FfiFileIoWriteError: Swift.Error {} /* Some bug in UniFFI... */
-extension FfiFileIoReadError: Swift.Error {} /* Some bug in UniFFI... */
+/* Some bug in UniFFI not marking the `uniffi::Error` as `Swift.Error`... */
+extension FfiFileIoWriteError: Swift.Error {}
+extension FfiFileIoReadError: Swift.Error {}
 
 struct FileHandleForWritingOutcome {
   let fileAlreadyExists: Bool
@@ -31,138 +31,6 @@ func fileForWriting(to absolutePath: String) -> FileHandleForWritingOutcome {
     result: .success(fileHandle)
   )
 
-}
-
-extension NetworkResponse {
-  init(data: Data, urlResponse: URLResponse) {
-    guard let httpUrlResponse = urlResponse as? HTTPURLResponse else {
-      fatalError("Expected URLResponse to always be HTTPURLResponse")
-    }
-    self.init(statusCode: UInt16(httpUrlResponse.statusCode), body: data)
-  }
-}
-
-extension FfiNetworkingOutcome {
-
-  static func fail(error: Swift.Error, data: Data? = nil, urlResponse: URLResponse? = nil) -> Self {
-    func message() -> String? {
-      data.map { String(data: $0, encoding: .utf8) } ?? nil
-    }
-    func statusCode() -> UInt16? {
-      urlResponse.map { $0 as? HTTPURLResponse ?? nil }?.map { UInt16($0.statusCode) } ?? nil
-    }
-
-    return .failure(
-      error: .requestFailed(
-        statusCode: statusCode(),
-        urlSessionUnderlyingError: String(describing: error),
-        errorMessageFromGateway: message()
-      )
-    )
-
-  }
-
-  static func with(
-    data: Data?,
-    urlResponse: URLResponse?,
-    error: Swift.Error?
-  ) -> Self {
-    if let error {
-      return .fail(error: error, data: data, urlResponse: urlResponse)
-    }
-    guard let data else { fatalError("If error is nil data SHOULD be present if error is nil.") }
-    guard let urlResponse else {
-      fatalError("Expected URLResponse to always be present if error is nil and data is some.")
-    }
-    return .success(value: NetworkResponse(data: data, urlResponse: urlResponse))
-  }
-}
-
-extension NetworkRequest {
-  // Convert `[Rust]NetworkRequest` to `[Swift]URLRequest`
-  func urlRequest(url: URL) -> URLRequest {
-    var request = URLRequest(url: url)
-    request.httpMethod = self.method
-    request.httpBody = self.body
-    request.allHTTPHeaderFields = self.headers
-    return request
-  }
-
-  func urlRequest() throws -> URLRequest {
-    guard let url = URL(string: self.url) else {
-      throw FfiNetworkingError.failedToCreateUrlFrom(string: self.url)
-    }
-    return self.urlRequest(url: url)
-  }
-}
-
-// Conform `[Swift]URLSession` to `[Rust]FfiNetworkingHandler`
-extension URLSession: FfiNetworkingHandler {
-  public func executeNetworkRequest(
-    request rustRequest: NetworkRequest,
-    listenerRustSide: FfiNetworkingOutcomeListener
-  ) throws {
-    guard let url = URL(string: rustRequest.url) else {
-      throw FfiNetworkingError.failedToCreateUrlFrom(string: rustRequest.url)
-    }
-    let task = dataTask(with: rustRequest.urlRequest(url: url)) { data, urlResponse, error in
-      let result = FfiNetworkingOutcome.with(
-        data: data,
-        urlResponse: urlResponse,
-        error: error
-      )
-      listenerRustSide.notifyOutcome(result: result)
-    }
-    task.resume()
-  }
-}
-
-public final class Async<Request, Intermediary, Response> {
-  typealias Operation = (Request) async throws -> Intermediary
-  typealias MapToResponse = (Intermediary) async throws -> Response
-
-  private var task: Task<Void, Never>?
-
-  let operation: Operation
-  let mapToResponse: MapToResponse
-
-  init(
-    operation: @escaping Operation,
-    mapToResponse: @escaping MapToResponse
-  ) {
-    self.operation = operation
-    self.mapToResponse = mapToResponse
-  }
-}
-
-extension Async: FfiNetworkingHandler
-where Request == NetworkRequest, Intermediary == (Data, URLResponse), Response == NetworkResponse {
-
-  convenience init(
-    call op: @escaping (URLRequest) async throws -> Intermediary
-  ) {
-    self.init(
-      operation: { (rustRequest: NetworkRequest) in try await op(rustRequest.urlRequest()) },
-      mapToResponse: { (data: Data, urlResponse: URLResponse) in
-        NetworkResponse(data: data, urlResponse: urlResponse)
-      }
-    )
-  }
-
-  public func executeNetworkRequest(
-    request rustRequest: NetworkRequest,
-    listenerRustSide: FfiNetworkingOutcomeListener
-  ) throws {
-    self.task = Task {
-      do {
-        let intermediary = try await self.operation(rustRequest)
-        let response = try await self.mapToResponse(intermediary)
-        listenerRustSide.notifyOutcome(result: .success(value: response))
-      } catch {
-        listenerRustSide.notifyOutcome(result: .fail(error: error))
-      }
-    }
-  }
 }
 
 public final class AsyncFileManager {
@@ -209,6 +77,24 @@ extension AsyncFileManager {
     }
   }
 
+}
+
+public final class Async<Request, Intermediary, Response> {
+  typealias Operation = (Request) async throws -> Intermediary
+  typealias MapToResponse = (Intermediary) async throws -> Response
+
+  private var task: Task<Void, Never>?
+
+  let operation: Operation
+  let mapToResponse: MapToResponse
+
+  init(
+    operation: @escaping Operation,
+    mapToResponse: @escaping MapToResponse
+  ) {
+    self.operation = operation
+    self.mapToResponse = mapToResponse
+  }
 }
 
 extension Async: FfiFileIoReadHandler
@@ -293,7 +179,6 @@ public final class CallbackBasedFileManager {
 }
 extension CallbackBasedFileManager: FfiFileIoReadHandler {
   func read(absolutePath: String, callback: @escaping (FfiFileIoReadOutcome) -> Void) {
-    print("🪲 SWIFT SimpleFileManager read: '\(absolutePath)'")
     guard let fileHandle = FileHandle(forReadingAtPath: absolutePath) else {
       return callback(
         FfiFileIoReadOutcome.success(value: .doesNotExist))
@@ -336,8 +221,6 @@ extension CallbackBasedFileManager: FfiFileIoWriteHandler {
     abortIfExists: Bool,
     callback: @escaping (FfiFileIoWriteOutcome) -> Void
   ) {
-    print("🐌 SWIFT SimpleFileManager write: '\(absolutePath)'")
-
     let fileHandleOutcome = fileForWriting(to: absolutePath)
     let alreadyExists = fileHandleOutcome.fileAlreadyExists
     if abortIfExists, alreadyExists {
@@ -381,9 +264,7 @@ extension CallbackBasedFileManager: FfiFileIoWriteHandler {
   }
 }
 
-func test_file_io_callbackbased(fileAbsolutePath: String) async throws {
-  print("🚀 SWIFT 'test_file_io_callbackbased' start")
-  defer { print("🏁 SWIFT 'test_file_io_callbackbased' done") }
+func test_callback(fileAbsolutePath: String) async throws {
   let fileIoInterface = FileIoInterface(
     fileWriter: CallbackBasedFileManager.shared,
     fileReader: CallbackBasedFileManager.shared
@@ -394,12 +275,10 @@ func test_file_io_callbackbased(fileAbsolutePath: String) async throws {
     extendStrategy: .prepend(separator: "\n"),
     contents: "Callback".data(using: .utf8)!
   )
-  print("✅ writeToNewOrExtendExistingFile CB outcome: \(outcome)")
+  print("✅🗂️  writeToNewOrExtendExistingFile CB outcome: \(outcome)")
 }
 
-func test_file_io_async(fileAbsolutePath: String) async throws {
-  print("🚀 SWIFT 'test_file_io_callbackbased' start")
-  defer { print("🏁 SWIFT 'test_file_io_callbackbased' done") }
+func test_async(fileAbsolutePath: String) async throws {
   let fileIoInterface = FileIoInterface(
     fileWriter: Async(call: AsyncFileManager.shared.write(contents:absolutePath:abortIfExists:)),
     fileReader: Async(call: AsyncFileManager.shared.read(absolutePath:))
@@ -410,43 +289,21 @@ func test_file_io_async(fileAbsolutePath: String) async throws {
     extendStrategy: .append(separator: "\n"),
     contents: "Async".data(using: .utf8)!
   )
-  print("✅ writeToNewOrExtendExistingFile ASYNC outcome: \(outcome)")
-}
-
-func test_file_io() async throws {
-  print("🚀 SWIFT 'test_file_io' start")
-  defer { print("🏁 SWIFT 'test_file_io' done") }
-  let fileAbsolutePath = "\(FileManager.default.currentDirectoryPath)/safeToRemove.txt"
-  try await test_file_io_callbackbased(fileAbsolutePath: fileAbsolutePath)
-  try await test_file_io_async(fileAbsolutePath: fileAbsolutePath)
-  try await test_file_io_callbackbased(fileAbsolutePath: fileAbsolutePath)
+  print("✅🗂️  writeToNewOrExtendExistingFile ASYNC outcome: \(outcome)")
 }
 
 func test() async throws {
-  print("🚀 SWIFT 'test' start")
-  defer { print("🏁 SWIFT 'test' done") }
-  try await test_file_io()
-  let urlSession = URLSession.shared
-
-  let clientCompletionCallbackBased = GatewayClient(
-    networkAntenna: urlSession
-  )
-
-  let clientAsyncBased = GatewayClient(
-    networkAntenna: Async(call: urlSession.data(for:))
-  )
-
-  // Call async method in Rust land from Swift!
-  let address = "account_rdx16xlfcpp0vf7e3gqnswv8j9k58n6rjccu58vvspmdva22kf3aplease"
+  print("🚀🗂️  SWIFT 'test_file_io' start")
+  defer { print("🏁🗂️  SWIFT 'test_file_io' done") }
+  let fileAbsolutePath = "\(FileManager.default.currentDirectoryPath)/safeToRemove.txt"
   do {
-    var balance = try await clientCompletionCallbackBased.getXrdBalanceOfAccount(address: address)
-    print("SWIFT ✅ completionCallbackBased balance: \(balance) ✅")
-    balance = try await clientAsyncBased.getXrdBalanceOfAccount(address: address)
-    print("SWIFT ✅ clientAsyncBased balance: \(balance) ✅")
+    try await test_callback(fileAbsolutePath: fileAbsolutePath)
+    try await test_async(fileAbsolutePath: fileAbsolutePath)
+    try await test_callback(fileAbsolutePath: fileAbsolutePath)
   } catch {
-    print("SWIFT ❌ getXrdBalanceOfAccount failed, error: \(String(describing: error))")
-  }
+    print("🚀 🗂️ SWIFT 'test_file_io' error: \(String(describing: error))")
 
+  }
 }
 
 try! await test()
